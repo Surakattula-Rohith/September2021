11b2d78c5d80e27088a4f232107a4a1f253f10ee
Surakattula Rohith <74316672+Surakattula-Rohith@users.noreply.github.com>
2021-09-16 10:26:43 +0530

    Update FACEDIR.cpp

--- FACEDIR.cpp
+++ FACEDIR.cpp
@@ -1,3 +1,7 @@
+// Chef is currently facing the north direction. Each second he rotates exactly 90 degrees in clockwise direction. Find  // the direction in which Chef is facing after exactly X seconds.
+
+
+
 #include <bits/stdc++.h>
 //#include<ext/pb_ds/assoc_container.hpp>
 //#include<ext/pb_ds/tree_policy.hpp>
